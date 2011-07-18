3c7066b039499302e73a6b73707b03539b2602aa
Varoun P <commits@varoun.com>
2011-07-18 16:36:27 +0530

    Added system definition for the API.

---
+++ code/web/api/defsys.lisp
@@ -0,0 +1,5 @@
+;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CL-USER; Base: 10 -*-
+
+(defsystem groklogs-webapi
+  (:default-pathname "./")
+  :members ("packages" "representations" "rest-frame"))
