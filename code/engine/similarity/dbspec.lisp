be3f9295fc6d753ffdb1cce5377e2e460e348935
Varoun P <commits@varoun.com>
2011-08-01 21:29:17 +0530

    Added code to setup a few special variables that hold table names, and initialise the related_items table.

--- code/engine/similarity/dbspec.lisp
+++ code/engine/similarity/dbspec.lisp
@@ -7,3 +7,21 @@
 
 ;;; The various tables.
 (defparameter *links-table-source* "links_normal")
+(defparameter *uid-map-table* "uid_map")
+(defparameter *iid-map-table* "iid_map")
+(defparameter *related-table* "related_items")
+
+;;; creating the related table.
+(defun initialise-related-items-table (&optional (dbspec *database-spec*)
+				       (table-name *related-table*))
+  (with-database (sqldb dbspec :if-exists :new)
+    (execute-command 
+     (format nil 
+	     "create table ~a (itemid integer, itemid_related integer, score float, unique
+(itemid, itemid_related))"
+	     table-name)
+     :database sqldb)
+    (execute-command (format nil "create index ~a on ~a (itemid, itemid_related)"
+			     (concatenate 'string table-name "_index")
+			     table-name)
+		     :database sqldb)))
